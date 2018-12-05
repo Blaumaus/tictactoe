3c3368eea10d35d41462097d0b2bdb041fb89ab6
havijman <margareq@protonmail.ch>
2018-12-05 22:39:06 +0200

    Updated main.js

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+// declaring constants
 const rl = require('readline-sync');
 const colors = require('colors');
 const win_coords = [[0,1,2], [0,3,6], [1,4,7], [3,4,5], [6,7,8], [2,5,8], [2,4,6], [0,4,8]];
